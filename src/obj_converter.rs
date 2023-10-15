139268e3810cdd93afde56e3a83c40eb912b6b3b
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-10-15 18:53:01 +0200

    write uvs if present

--- src/obj_converter.rs
+++ src/obj_converter.rs
@@ -10,6 +10,7 @@ where
 {
     geo_schema.parse_primitives();
     geo_schema.parse_point_attributes();
+    geo_schema.parse_vertex_attributes();
 
     let p_attr = if let Some(GeoAttributeKind::Float64(x)) = geo_schema.point_attribute("P") {
         x
@@ -17,6 +18,7 @@ where
         panic!("no p float3 attr");
     };
 
+    //writing P (Cd)
     match geo_schema.point_attribute("Cd") {
         Some(GeoAttributeKind::Float64(cd_attr)) => {
             for ptnum in 0..geo_schema.point_count() {
@@ -42,14 +44,58 @@ where
         _ => panic!("Cd present, but not a float3 attr"),
     };
 
-    for prim in geo_schema.polygons() {
-        file.write(b"f").expect("io error");
-
-        // obj expects opposite winding order, and vertex indices start at 1, no 0
-        for vtx in prim.vertices.iter().rev() {
-            file.write(format!(" {}", vtx.ptnum + 1).as_bytes())
-                .expect("io error");
+    // writing uv
+    let have_uvs = if let Some(GeoAttributeKind::Float64(uv_attr)) = geo_schema.vertex_attribute("uv") {
+        match uv_attr.tuple_size(){
+            3 => {
+                for vtxnum in 0..geo_schema.vertex_count() {
+                    let uv = uv_attr.value(vtxnum);
+                    file.write(format!("vt {} {} {}\n", uv[0], uv[1], uv[2]).as_bytes())
+                        .expect("io error");
+                }
+                true
+            }
+            2 => {
+                for vtxnum in 0..geo_schema.vertex_count() {
+                    let uv = uv_attr.value(vtxnum);
+                    file.write(format!("vt {} {} 0\n", uv[0], uv[1]).as_bytes())
+                        .expect("io error");
+                }
+                true
+            }
+            x => {
+                println!("float uv attribute found, but it's of unexpected size {}, skipping", x);
+                false
+            }
         }
-        file.write(b"\n").expect("io error");
+    } else { false };
+
+    // writing faces
+    if have_uvs {
+        for prim in geo_schema.polygons() {
+            file.write(b"f").expect("io error");
+
+            // obj expects opposite winding order starting at same vertex, and vertex indices start at 1, no 0
+            let vtxcount = prim.vertices.len();
+            for i in 0..vtxcount {
+                let vtx = &prim.vertices[(vtxcount-i)%vtxcount];
+                file.write(format!(" {}/{}", vtx.ptnum + 1, vtx.vtxnum + 1).as_bytes())
+                    .expect("io error");
+            }
+            file.write(b"\n").expect("io error");
+        }
+    } else {
+        for prim in geo_schema.polygons() {
+            file.write(b"f").expect("io error");
+
+            // obj expects opposite winding order starting at same vertex, and vertex indices start at 1, no 0
+            let vtxcount = prim.vertices.len();
+            for i in 0..vtxcount {
+                let vtx = &prim.vertices[(vtxcount-i)%vtxcount];
+                file.write(format!(" {}", vtx.ptnum + 1).as_bytes())
+                    .expect("io error");
+            }
+            file.write(b"\n").expect("io error");
+        }
     }
 }
